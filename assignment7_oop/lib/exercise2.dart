782a7b2d3e37155213b211ad6b8a021a35b3c1ae
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-08-05 23:17:21 +0300

    create Customer class

--- assignment7_oop/lib/exercise2.dart
+++ assignment7_oop/lib/exercise2.dart
@@ -1,4 +1,3 @@
-// class to represent a person
 class Person {
   String? name;
   String? email;
@@ -7,10 +6,10 @@ class Person {
 }
 
 class Author extends Person {
-  static int id = 1;
-  
+  static int id = 0;
+
   Author({required super.name, required super.email}) {
-    id++;
+    id++; // increment each time object of type Author gets created
   }
 }
 
@@ -21,4 +20,30 @@ class Book {
   Author? author;
 
   Book({required this.name, required this.ISBN, required this.editionNumber, required this.author});
+}
+
+class Customer extends Person {
+  List<Book>? bookList;
+
+  Customer({required super.name, required super.email}) {
+    bookList = [];
+  }
+
+  void addBook(Book book) {
+    bookList!.add(book);
+    print("Book ${book.name} by ${book.author!.name} is added to your list ✅");
+  }
+
+  void printInformation() {
+    print("Customer Name : $name | Customer Email : $email");
+    if (bookList == null || bookList!.isEmpty) {
+      print("You haven't added any books yet.");
+    } else {
+      for (var book in bookList!) {
+        print("Book name : ${book.name} by ${book.author?.name}");
+        print("Book ISBN : ${book.ISBN}");
+        print("Contact the author by email : ${book.author?.email}");
+      }
+    }
+  }
 }
