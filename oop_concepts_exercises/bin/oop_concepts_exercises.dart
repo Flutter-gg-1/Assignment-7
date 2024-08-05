35ebdc1ad15971d73db5fa5c2600de0c1f7de733
Basel-75 <basel_1422@outlook.com>
2024-08-06 00:02:16 +0300

    Create : Dart project

---
+++ oop_concepts_exercises/bin/oop_concepts_exercises.dart
@@ -0,0 +1,5 @@
+import 'package:oop_concepts_exercises/oop_concepts_exercises.dart' as oop_concepts_exercises;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oop_concepts_exercises.calculate()}!');
+}
