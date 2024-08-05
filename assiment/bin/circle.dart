a9eeb8536748b4410a8b961b6e391fd27be8e5c2
muhannad <mohnsalzaher@gmail.com>
2024-08-05 21:34:55 +0300

    Creating Circle class and override the methoeds and using git and set

---
+++ assiment/bin/circle.dart
@@ -0,0 +1,33 @@
+import 'exercise.dart';
+import 'dart:math';
+
+class Circle extends Shape {
+  late double _radius;
+  static const double pi = 3.1415926535897932;
+
+  Circle(this._radius);
+
+  Circle.noAttribute() {
+    _radius = 2;
+  }
+
+  @override
+  void getArea() {
+    double area = pi * _radius * _radius;
+    print("circle area is: $area");
+  }
+
+  @override
+  void getPerimeter() {
+    var perimeter = 2 * pi * _radius;
+    print("Circle perimeter is: $perimeter");
+  }
+
+  double get raduis {
+    return _radius;
+  }
+
+  set radius(double radius) {
+    _radius = radius;
+  }
+}
