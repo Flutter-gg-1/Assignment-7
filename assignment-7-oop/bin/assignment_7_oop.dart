d096df4cfec21996518cb9c2897041c11ef5db69
turki-aloufi <turkialoufi44@gmail.com>
2024-08-05 19:31:25 +0300

    initial commit

---
+++ assignment-7-oop/bin/assignment_7_oop.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_7_oop/assignment_7_oop.dart' as assignment_7_oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment_7_oop.calculate()}!');
+}
---
+++ assignment-7-oop/test/assignment_7_oop_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_7_oop/assignment_7_oop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
