419b152f32f8872d645a97ca1b1fd988e110ba33
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-05 17:25:27 +0300

    create Circle class

---
+++ exercise1/exercise1/circle.dart
@@ -0,0 +1,25 @@
+import 'shape.dart';
+import 'dart:math' as math;
+
+class Circle extends Shape {
+  late num _radius;
+  static double pi = math.pi / 12;
+
+  Circle({required radius}) : _radius = radius;
+  Circle.noAttribute() : _radius = 2;
+
+  get getRadius => _radius;
+  set setRadous(num r) => _radius = r;
+
+  @override
+  void getArea() {
+    num area = math.pow(_radius, 2) * pi;
+    print("circle area is: $area");
+  }
+
+  @override
+  void getPermiter() {
+    num perimeter = 2 * pi * _radius;
+    print("circle perimeter is: $perimeter");
+  }
+}
