026c78a55d2628ee6b00b1079f2a5a335eded2a8
muhannad <mohnsalzaher@gmail.com>
2024-08-06 00:14:22 +0300

    add Auth in input and error handling and print the ID

--- assiment/bin/assiment.dart
+++ assiment/bin/assiment.dart
@@ -1,14 +1,48 @@
 import 'circle.dart';
 import 'rectangle.dart';
+import 'book.dart';
+import 'dart:io';
+import 'author.dart';
 
 void main(List<String> arguments) {
-  Circle c1 = Circle(5);
-  Circle c2 = Circle.noAttribute();
-  c1.getArea();
-  c1.getPerimeter();
-  print("Radius of c2: ${c2.raduis}");
-  Rectangle r1 = Rectangle(3, 5);
-  r1.getArea();
-  r1.width = 8;
-  r1.getPerimeter();
+  // Circle c1 = Circle(5);
+  // Circle c2 = Circle.noAttribute();
+  // c1.getArea();
+  // c1.getPerimeter();
+  // print("Radius of c2: ${c2.raduis}");
+  // Rectangle r1 = Rectangle(3, 5);
+  // r1.getArea();
+  // r1.width = 8;
+  // r1.getPerimeter();
+  List<Author> auth = [];
+  int input;
+  List count = [];
+  try {
+    while (true) {
+      print("Kindly enter number 1 to add Author and 2 to quit the app ");
+      input = int.parse(stdin.readLineSync()!);
+      if (input == 1) {
+        switch (input) {
+          case 1:
+            print("add auther: ");
+            String auther = stdin.readLineSync()!;
+            print("add Email: ");
+            String email = stdin.readLineSync()!;
+
+            var authers = Author(name: auther, email: email);
+
+            count.add(Author.id);
+            auth.add(authers);
+        }
+      } else if (input == 2) {
+        break;
+      }
+    }
+  } catch (error) {
+    print(error);
+  }
+
+  for (var element in count) {
+    print("Authe$element");
+  }
 }
