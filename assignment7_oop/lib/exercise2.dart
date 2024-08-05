038a40ca8dae2f5a153fb3249fb936142149628a
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-08-05 20:06:58 +0300

    constructor for Person class & create Author class

--- assignment7_oop/lib/exercise2.dart
+++ assignment7_oop/lib/exercise2.dart
@@ -2,4 +2,14 @@
 class Person {
   String? name;
   String? email;
+
+  Person({required this.name, this.email});
+}
+
+class Author extends Person {
+  static int id = 1;
+  
+  Author({required super.name, required super.email}) {
+    id++;
+  }
 }
