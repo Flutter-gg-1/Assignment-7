8c8b77bcb7795febce52332e4590217ae9fef170
MAHA Al-ATAWI <mem880.a@gmail.com>
2024-08-05 18:58:36 +0300

    Dart files

---
+++ assignment07/bin/assignment07.dart
@@ -0,0 +1,5 @@
+import 'package:assignment07/assignment07.dart' as assignment07;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment07.calculate()}!');
+}
---
+++ assignment07/test/assignment07_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment07/assignment07.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
