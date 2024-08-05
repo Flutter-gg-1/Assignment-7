23d3a2c426df3cb16330d79fd065ed61661d2ead
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-05 17:57:43 +0300

    complete excercise 1

--- exercise1/bin/exercise1.dart
+++ exercise1/bin/exercise1.dart
@@ -3,9 +3,9 @@ import '../exercise1/circle.dart';
 
 void main(List<String> arguments) {
   //Excercise 1
-  print("* " * 5);
+  print("* " * 12);
   print("Welcom to my calculator");
-  print("* " * 5);
+  print("* " * 12);
   print("*****Circle*****");
   Circle c1 = Circle(radius: 5);
   Circle c2 = Circle.noAttribute();
@@ -13,5 +13,19 @@ void main(List<String> arguments) {
   c1.getPermiter();
 
   print("The Radius of c2 is ${c2.getRadius}");
+
+  print("*****Rectangle *****");
+
+  Rectangle r1 = Rectangle(length: 3, width: 5);
+
+  r1.getArea();
+  r1.getPermiter();
+
+  print("chage legth to 6");
+  r1.setLength = 6;
+  r1.getPermiter();
+
+  //end of Excercise 1
+
   
 }
