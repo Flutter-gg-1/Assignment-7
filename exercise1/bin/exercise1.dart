67784d550d51441ce016102c48caec55e3b6de65
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-05 17:50:05 +0300

    add circle part in main

--- exercise1/bin/exercise1.dart
+++ exercise1/bin/exercise1.dart
@@ -1,2 +1,16 @@
+import '../exercise1/rectangle.dart';
+import '../exercise1/circle.dart';
+
 void main(List<String> arguments) {
+  //Excercise 1
+  print("* " * 5);
+  print("Welcom to my calculator");
+  print("* " * 5);
+  print("*****Circle*****");
+  Circle c1 = Circle(radius: 5);
+  Circle c2 = Circle.noAttribute();
+  c1.getArea();
+  c1.getPermiter();
+
+  print("The Radius of c2 is ${c2.getRadius}");
 }
