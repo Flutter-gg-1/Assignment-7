c795afea481e38971d938fdbe9d2886ed2d163b0
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-08-06 00:34:21 +0300

    adding user input

--- assignment7/bin/assignment7.dart
+++ assignment7/bin/assignment7.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'exercise1.dart';
 import 'exercise2.dart';
 
@@ -16,22 +18,32 @@ void main() {
   r1.getArea();
   print("-------------------------------");
 
+  Author author1 = Author("abdullah", "abdullah@gmail.com");
+  Author author2 = Author("suad", "suad@gmail.com");
+  Author author3 = Author("noor", "noor@gmail.com");
+
+  Book book1 = Book("khalid", "2", 1, author1);
+  Book book2 = Book("najwa", "443", 4, author2);
+  Book book3 = Book("ahmed", "56", 6, author3);
+
+  String? customerName;
+  String? customerEmail;
+
   try {
-    Author author1 = Author("abdullah", "abdullah@gmail.com");
-    Author author2 = Author("suad", "suad@gmail.com");
-    Author author3 = Author("noor", "noor@gmail.com");
-
-    Book book1 = Book("khalid", "2", 1, author1);
-    Book book2 = Book("najwa", "443", 4, author2);
-    Book book3 = Book("ahmed", "56", 6, author3);
-
-    Customer customer1 = Customer("sara", "sara@customer.com");
-    customer1.addBook(book1);
-    customer1.addBook(book2);
-    customer1.addBook(book3);
-
-    customer1.printInformation();
+    print("Please enter customer name: ");
+    customerName = stdin.readLineSync();
+    print("Please enter customer email: ");
+    customerEmail = stdin.readLineSync();
   } catch (error) {
-    print("somthing went wrong");
+    print("opps there is an error");
   }
+
+  Customer customer1 = Customer(customerName!, customerEmail!);
+
+
+  customer1.addBook(book1);
+  customer1.addBook(book2);
+  customer1.addBook(book3);
+
+  customer1.printInformation();
 }
