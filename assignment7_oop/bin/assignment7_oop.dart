e20a1edbaf30b025aa88d4bf5b043d0fc686db30
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-08-05 19:12:57 +0300

    finish exercise 1

--- assignment7_oop/bin/assignment7_oop.dart
+++ assignment7_oop/bin/assignment7_oop.dart
@@ -1,3 +1,30 @@
+import 'package:assignment7_oop/exercise1.dart'; // exercise 1 file
+
 void main() {
-  print('Assignment 7 : oop');
+  print('====(> Exercise 1 <)====');
+  // Create 2 objects of the Circle class named c1 and c2:
+  // c1 with a radius of 5.
+  // c2 using the named constructor without a radius parameter.
+  Circle c1 = Circle(radius: 5);
+  Circle c2 = Circle.noAttribute();
+
+  // Execute getArea() using c1.
+  c1.getArea();
+  // Execute getPerimeter() using c1.
+  c1.getPerimeter();
+  // Print the radius of c2.
+  print(c2.getRadius);
+  // Create an object of the Rectangle class named r1 with a length of 3 and a width of 5.
+  Rectangle r1 = Rectangle(length: 3, width: 5);
+  // Execute getArea() using r1.
+  r1.getArea();
+  // Modify either the length or width (but not both) to make the perimeter equal to 22.
+  r1.setLength = 6;
+  // Execute getPerimeter() using r1.
+  r1.getPerimeter();
+
+  print('--'*20);
+
+  print('====(> Exercise 2 <)====');
+  
 }
