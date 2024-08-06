359b0d73191b7041cce17dbecbf83a117264177d
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-06 07:01:36 +0300

    Done exercise2

--- exercise1/bin/exercise2.dart
+++ exercise1/bin/exercise2.dart
@@ -2,13 +2,15 @@ import 'all_file.dart';
 
 void exercise2() {
   print('\n ________####### Exercise 2 #######________\n');
-  Author author1 = Author(name: 'Abdullah', email: 'email');
-  Author author2 = Author(name: 'Abdullah', email: 'email');
-  Author author3 = Author(name: 'Abdullah', email: 'email');
-  print(Author.id);
+  Author author1 = Author(name: '1. Abdullah', email: 'email');
+  Author author2 = Author(name: '2. Mukhlef', email: 'email');
+  Author author3 = Author(name: '3. AL-Shammari', email: 'email');
+  print('Number of authors are: ${Author.id}');
+  print('Authors are: ${author1.name}, ${author2.name}, ${author3.name}');
+  print('');
   Book book1 =
       Book(name: 'book1', iISBN: '123', editionNumber: 1, author: author1);
-  Customer customer1 = Customer(name: 'customer1', email: 'email');
+  Customer customer1 = Customer(name: 'Abu_Mukhlef', email: 'Abu_Mukhlef@hotmail.com');
   customer1.addBook(book1);
   customer1.printCustomerInformation();
   showPrompt();
--- exercise1/bin/object_of_books.dart
+++ exercise1/bin/object_of_books.dart
@@ -1,21 +1,15 @@
 class Person {
   String? name;
   String? email;
-  Person({required this.name,required this.email});
+  Person({required this.name, required this.email});
 }
 
 class Author extends Person {
-  static int  id = 0;
+  static int id = 0;
 
-  Author({required super.name,required super.email}) {
+  Author({required super.name, required super.email}) {
     id++;
   }
-
-  void printAuthorInformation() {
-    print('Name: $name');
-    print('Email: $email');
-  }
-
 }
 
 class Book {
@@ -24,21 +18,26 @@ class Book {
   int? editionNumber;
   Author? author;
 
-  Book({required this.name,required  this.iISBN,required  this.editionNumber,required this.author});
+  Book(
+      {required this.name,
+      required this.iISBN,
+      required this.editionNumber,
+      required this.author});
 }
 
 class Customer extends Person {
-  List<Book> books = [];
+  List<Book> purchasedBooks = [];
 
-  Customer({required super.name,required super.email});
+  Customer({required super.name, required super.email});
 
   void addBook(Book book) {
-    books.add(book);
+    purchasedBooks.add(book);
   }
 
   void printCustomerInformation() {
     print('Name: $name');
     print('Email: $email');
-    print('Books: $books');
+    print('Books: ${purchasedBooks.first.name}');
+    print('Authors: ${Author.id}');
   }
 }
